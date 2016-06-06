5c253e8238b68c419b4dacb625e6d88f62f3c506
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2016-06-06 16:16:37 +0200

    extend type to handle ties and weights, predict method

--- src/SmoothingSplines.jl
+++ src/SmoothingSplines.jl
@@ -1,8 +1,8 @@
 module SmoothingSplines
 
-import StatsBase: fit!, fit, RegressionModel
+import StatsBase: fit!, fit, RegressionModel, rle, ordinalrank
 
-export SmoothingSpline, fit!, fit
+export SmoothingSpline, fit!, fit, predict
 
 
 LAPACKFloat = Union{Float32,Float64}
@@ -10,43 +10,46 @@ LAPACKFloat = Union{Float32,Float64}
 include("matrices.jl")
 
 type SmoothingSpline{T<:LAPACKFloat} <: RegressionModel
-    X::Vector{T}
-    Y::Vector{T} # eventually use GLM response objects?
-    perm::Vector{Int}
-    h::Vector{T}
+    Xorig::Vector{T} # original grid points, but sorted
+    Yorig::Vector{T} # original values, sorted according to x
+    Xrank::Vector{Int}
+    Xdesign::Vector{T}
+    Xcount::Vector{Int} # to how many observations does X correspond?
+    weights::Vector{T}
     RpαQtQ::Matrix{T} # in symmetric banded matrix format
     g::Vector{T} # fitted values
     γ::Vector{T} # 2nd derivatives of fitted vals
     λ::T
 end
 
-function fit{T<:LAPACKFloat}(::Type{SmoothingSpline}, X::Vector{T}, Y::Vector{T}, λ::T)
-    idx = sortperm(X)
-    X = X[idx]
-    n = length(X)
-    Y = Y[idx]
-    h = diff(X)
+function fit{T<:LAPACKFloat}(::Type{SmoothingSpline}, X::Vector{T}, Y::Vector{T}, λ::T, ws=ones(Y))
+    Xrank = ordinalrank(X) # maybe speed this up when already sorted
+    Xorig = sort(X)
+    Yorig = sort(Y)
 
-    RpαQtQ = QtQpR(h, λ)
+    Xdesign, Xcount = rle(Xorig)
+
+    RpαQtQ = QtQpR(diff(Xdesign), λ)
     pbtrf!('U', 2, RpαQtQ)
 
-    spl = SmoothingSpline{T}(X, Y, idx, h, RpαQtQ, zeros(Y), zeros(length(Y)-2), λ)
+    spl = SmoothingSpline{T}(Xorig, Yorig, Xrank, Xdesign, Xcount, ws, RpαQtQ, zeros(Y), zeros(length(Y)-2), λ)
     fit!(spl)
-
 end
 
 function fit!{T<:LAPACKFloat}(spl::SmoothingSpline{T})
-    Y = spl.Y
-    g = spl.g
-    n = length(spl.Y)
-    h = spl.h
+
+    g = spl.g #store Y in g initially
+    running_rle_mean!(g, spl.Yorig, spl.Xcount)
+    Y = copy(g)
+    n = length(spl.g)
+    h = diff(spl.Xdesign)
     λ = spl.λ
     Q = ReinschQ(h)
 
     RpαQtQ = spl.RpαQtQ
-    QtY = At_mul_B!(spl.γ, Q, Y)
-    pbtrs!('U', 2, RpαQtQ, QtY)
-    A_mul_B!(g, Q, QtY)
+    γ = At_mul_B!(spl.γ, Q, g)
+    pbtrs!('U', 2, RpαQtQ, γ)
+    A_mul_B!(g, Q, γ)
     broadcast!(*, g, g, λ)
     broadcast!(-,g, Y, g)
     spl
@@ -55,6 +58,32 @@ end
 function fit!{T<:LAPACKFloat}(spl::SmoothingSpline{T}, Y::Vector{T})
     spl.Y = Y[spl.idx]
     fit!(spl)
+end
+
+
+function predict{T<:LAPACKFloat}(spl::SmoothingSpline{T})
+    # need to convert back from RLE encoding
+    Xcount = spl.Xcount
+    curridx = 1::Int
+    g = zeros(length(spl.Yorig))
+    @inbounds for i=1:length(Xcount)
+        @inbounds   for j=1:Xcount[i]
+            g[curridx] = spl.g[i]
+            curridx += 1
+        end
+    end
+    g[spl.Xrank]
+end
+
+# TODO: this mean should be weighted for weighted case
+function running_rle_mean!{T<:Real}(g::Vector{T}, Y::Vector{T}, rlecount::Vector{Int64})
+  length(g) == length(rlecount) ||  throw(DimensionMismatch())
+  curridx = 1::Int
+  for i=1:length(rlecount)
+    g[i] = mean(Y[curridx:(curridx+rlecount[i]-1)])
+    curridx += rlecount[i]
+  end
+  g
 end
 
 end # module
